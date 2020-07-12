489b1435dc1369f68f90356dd89e319511710eea
ahmed_bahaa <a.bahaarefaii@gmail.com>
2020-07-13 00:33:22 +0200

    listing in different ways

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,29 @@
+import 'package:MyShop/models/product.dart';
 import 'package:flutter/material.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+import './providers/product_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        accentColor: Colors.greenAccent[400],
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          accentColor: Colors.greenAccent[400],
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName : (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/product_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
   @override
   Widget build(BuildContext context) {
     final productID = ModalRoute.of(context).settings.arguments as String ; 
+    final loadedProduct = Provider.of<Products>(context).findById(productID);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(productID),
+        title: Text(loadedProduct.title),
       ),
     );
   }

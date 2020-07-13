f20c568141616210d3dae1f3cd7e325d4f05fc3a
ahmed_bahaa <a.bahaarefaii@gmail.com>
2020-07-13 22:44:37 +0200

    adding cart prvider

---
+++ lib/providers/cart.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  @required
+  final String id;
+  @required
+  final String title;
+  @required
+  final int quantity;
+  @required
+  final double price;
+
+  CartItem({this.id, this.title, this.price, this.quantity});
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(
+    String productId,
+    double price,
+    String title,
+  ) {
+    if (_items.containsKey(productId)) {
+      _items.update(productId, (existedProduct) => CartItem(
+        id: existedProduct.id,
+        title: existedProduct.title,
+        price: existedProduct.price,
+        quantity: existedProduct.quantity + 1,
+      ));
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+  }
+}

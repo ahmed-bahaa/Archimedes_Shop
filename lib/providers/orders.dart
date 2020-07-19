629302ef0fd77bd092ccd62c4f10a9ec77957311
ahmed_bahaa <a.bahaarefaii@gmail.com>
2020-07-20 00:19:49 +0200

    Fetching Orders & Fixing an Issue

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -24,21 +24,52 @@ class Order with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    const url = "https://flutter-202006.firebaseio.com/orders.json";
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if ( extractedData == null ){
+      return null;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(OrderItem(
+        id: orderId,
+        amount: orderData['amount'],
+        dateTime: DateTime.parse(orderData['dateTime']),
+        products: (orderData['products'] as List<dynamic>)
+            .map((item) => CartItem(
+              id: item['id'], 
+              price: item['price'],
+              quantity: item['quantity'],
+              title: item['title'],
+            ),)
+            .toList(),
+      ));
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrders(List<CartItem> cartProducts, double total) async {
     const url = "https://flutter-202006.firebaseio.com/orders.json";
     final timeStamp = DateTime.now();
 
-    final response = await http.post(url, body: json.encode({
-      'amount' : total,
-      'dateTime' : timeStamp.toIso8601String(),
-      'products' : cartProducts.map((cp) => {
-        'id' : cp.id,
-        'title' : cp.title,
-        'quantity' : cp.quantity,
-        'price' : cp.price,
-      }).toList(),
-
-    }),);
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': timeStamp.toIso8601String(),
+        'products': cartProducts
+            .map((cp) => {
+                  'id': cp.id,
+                  'title': cp.title,
+                  'quantity': cp.quantity,
+                  'price': cp.price,
+                })
+            .toList(),
+      }),
+    );
 
     _orders.insert(
       0,
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,8 +4,29 @@ import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 import '../providers/orders.dart' show Order;
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = "/orders";
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  var _isLoading = false;
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Order>(context, listen: false).fetchAndSetOrders();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final ordersData = Provider.of<Order>(context);
@@ -14,9 +35,9 @@ class OrdersScreen extends StatelessWidget {
         title: Text("Your Orders"),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
+      body: _isLoading ? Center(child: CircularProgressIndicator()) : ListView.builder(
         itemCount: ordersData.orders.length,
-        itemBuilder: ( ctx , i) => OrderItem(ordersData.orders[i]),
+        itemBuilder: (ctx, i) => OrderItem(ordersData.orders[i]),
       ),
     );
   }
